d812c17c14696552cf8b2c98b92db967dd1e6a14
Lesly Tineo <94697931+LeslyS05@users.noreply.github.com>
2023-07-14 17:43:44 -0400

    Add files via upload

---
+++ 2 - PUBLICACIONES/Publicar_LeslyTineo.m
@@ -0,0 +1,42 @@
+%% Lesly Scarlet Tineo Franco ID1108027
+
+% Mi carrera es Ciencia de datos, escogí estudiar esta carrera debido a que
+% su campo de aplicación es uno de los amplios, debido a su novedad la
+% comunidad en internet respecto a esta carrera es muy activa y competitiva
+% lo que ofrece multiples oportunidades de crecimiento personal y
+% desarrollo de la materia. 
+% Esta carrera también presenta salarios muy competitivos y con buenas
+% oportunidades de empleo en espacios que ofrecen crecimiento y
+% flexibilidad social. Me encanta que varios de los puestos disponibles
+% para esta carrera sean totalmente virtuales o semipresenciales. 
+%
+% |Mi interés en estudiar MATLAB surge a partir de conocer a una profesora
+% que me recomendó estudiar el lenguaje, este impulso inicial fue
+% respaldado por varios articulos y videos de youtube que resaltaban que
+% Matlab es útil en el manejo de bases de datos, visualizaciones y creación
+% de modelos IA. Además, debido que no se encuentra en el top 10 de
+% lenguajes más utilizados esto significa que los empleadores que necesitan
+% una persona especializada en este lenguaje tiene un poco más de
+% dificultades para encontrar a un candidato adecuado. Me gusta mucho la
+% posibilidad de tener matrices en más de dos dimensiones y creo que si
+% aprendo bien a utilizar la plataforma esta parte lo puede hacer muy
+% interesante|
+%
+%
+%% Diferencia entre las ecuaciones del seno y el coseno
+% 
+% $\sin(\theta) = \frac{{\text{{longitud del cateto opuesto}}}}{{\text{{longitud de la hipotenusa}}}}$
+% $\cos(\theta) = \frac{{\text{{longitud del cateto adyacente}}}}{{\text{{longitud de la hipotenusa}}}}$
+%
+%% Gráfica de coseno
+
+t = 0:pi:2*pi; 
+
+figure; 
+plot(cos(t));
+
+% Para mejroar esta gráfica es posible: etiquetar los ejes, colocar un
+% título para la grafica, cambiar el estilo de línea o cambiar los colores.
+% En el caso de la forma V de la grafica es posible reducir el tamaño del
+% saldo, diviendo pi por un numero grande para que la curva aparezca más
+% suave.
