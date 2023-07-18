b1aa62317ed95b819a4fe508305e0b01efd5aa07
Lesly Tineo <94697931+LeslyS05@users.noreply.github.com>
2023-07-17 23:56:52 -0400

    Update proyecto_final_LeslyTineo.m

--- 8 - PROYECTO FINAL/proyecto_final_LeslyTineo.m
+++ 8 - PROYECTO FINAL/proyecto_final_LeslyTineo.m
@@ -38,3 +38,20 @@ SF_Guerrero = data_Guerrero(:, 22);
 B2_Guerrero = data_Guerrero(:, 10);
 B3_Guerrero = data_Guerrero(:, 11);
 HR_Guerrero = data_Guerrero(:, 12);
+
+%% Función
+% Esta funcion solicitada se encuentra en la carpeta de funciones, al
+% momento de probar el codigo, por favor recordar colocar el path correcto
+% para que el programa funcione.
+
+%% Calcular factores BA, OBP y SLG
+%
+
+% *Factores BA, OBP y SLG de Albert Pujols* 
+[BA_Pujols, OBP_Pujols, SLG_Pujols] = calcular_factores(H_Pujols, AB_Pujols, BB_Pujols, HBP_Pujols, SF_Pujols, B2_Pujols, B3_Pujols, HR_Pujols);
+
+% *Factores BA, OBP y SLG de David Ortiz*
+[BA_Ortiz, OBP_Ortiz, SLG_Ortiz] = calcular_factores(H_Ortiz, AB_Ortiz, BB_Ortiz, HBP_Ortiz, SF_Ortiz, B2_Ortiz, B3_Ortiz, HR_Ortiz);
+
+% *Factores BA, OBP y SLG de Vladimir Guerrero*
+[BA_Guerrero, OBP_Guerrero, SLG_Guerrero] = calcular_factores(H_Guerrero, AB_Guerrero, BB_Guerrero, HBP_Guerrero, SF_Guerrero, B2_Guerrero, B3_Guerrero, HR_Guerrero);
